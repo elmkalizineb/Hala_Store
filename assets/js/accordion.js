f0ec61c21e8c1ae602557f1c131280c32f6bc3b9
elmkalizineb <elmkalizineb@gmail.com>
2026-02-20 18:21:40 +0100

    returns page

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,1 +1,26 @@
 /*=============== ACCORDION ===============*/
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach((item) =>{
+    const header = item.querySelector('.accordion-header'),
+    content = item.querySelector('.accordion-content'),
+    icon = item.querySelector('.accordion-header i');
+
+    header.addEventListener('click' , () =>{
+        const isOpen = item.classList.toggle('accordion-open');
+
+        content.style.height=isOpen ? content.scrollHeight + 'px' : '0';
+
+        icon.className = isOpen ? 'ri-subtract-line' : 'ri-add-line';
+
+        accordionItems.forEach((otherItem) =>{
+            if(otherItem !== item && otherItem.classList .contains('accordion-open')){
+                otherItem.querySelector('.accordion-content').style.height = '0';
+
+                otherItem.querySelector('.accordion-header i').className = 'ri-add-line';
+
+                otherItem.classList.remove = 'accordion-open';
+            }
+        });
+    });
+});
